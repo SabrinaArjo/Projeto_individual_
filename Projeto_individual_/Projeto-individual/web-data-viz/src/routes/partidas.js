4d4ce61d0d348310db8b80d0846158eda9bdf2d1
SabrinaArjo <sabrina.araujo@sptech.school>
2025-12-02 20:37:54 -0300

    Arrumando dash

--- Projeto_individual_/Projeto-individual/web-data-viz/src/routes/partidas.js
+++ Projeto_individual_/Projeto-individual/web-data-viz/src/routes/partidas.js
@@ -4,7 +4,9 @@ var router = express.Router();
 var partidaController = require("../controllers/partidaController");
 
 router.post("/registrar", partidaController.registrar);
+
 router.get("/listar/:idUsuario", partidaController.listarPorUsuario);
+
 router.get("/dashboard/:idUsuario", partidaController.dashboard);
 
 module.exports = router;
